44d702ebae49c740b73cf5287f1105a7a8a68aac
Antonio Júnior <juniorssz@gmail.com>
2025-08-10 20:29:58 -0300

    feat(config): add initial config and paths modules

---
+++ src/db_scraper/config.py
@@ -0,0 +1,19 @@
+from . import paths
+
+"""
+Centralizes the application settings, such as URLs,
+file names, model parameters, and other constants.
+"""
+
+# Base headers that do not change
+BASE_HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
+    'X-Requested-With': 'XMLHttpRequest',
+}
+
+# Template for the playlist view URL
+BASE_PLAYLIST_URL_TEMPLATE = "https://discografiabrasileira.com.br/playlists/{playlist_id}"
+
+# API Templates
+API_TRACKLIST_URL_TEMPLATE = "https://discografiabrasileira.com.br/fonograma/@relationById/{playlist_id}/@type/MusicRecording/@orderBy/@.@order/@orderDir/asc/@pp/50/p/1?shiro_content=true"
+API_CONTENT_URL_TEMPLATE = "https://discografiabrasileira.com.br/api/1.0/content/{data_id}?fields=_id,name,audio[contentUrl;duration],creator[_id;name],recordingOf[_id;name;author[_id;name]]"
---
+++ src/db_scraper/paths.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+"""
+Centralizes the definition of all important directory and file paths for the project.
+"""
+
+# --- Core Paths ---
+PROJECT_ROOT = Path(__file__).resolve().parents[2]
+SRC_DIR = PROJECT_ROOT / "src"
+PACKAGE_DIR = SRC_DIR / "db-scraper"
+OUTPUTS_DIR = PROJECT_ROOT / "outputs"
+
+# --- Specific Output Paths ---
+MUSICS_DIR = OUTPUTS_DIR / "musics"
+
+
